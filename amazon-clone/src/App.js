7dd2684035114574485be5c9de23f4235bf4f652
adithyaabinoy <adithyaabinoy@gmail.com>
2023-05-31 19:56:23 +0530

    header1

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello ,lets build amazon store</h1>
+      <Header />
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
