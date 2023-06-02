901666d1df8d7ce9dad473f1eef82c44d8fb0fbc
adithyaabinoy <adithyaabinoy@gmail.com>
2023-06-02 17:12:58 +0530

    products

---
+++ amazon-clone/src/Product.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Product.css';
+
+function Product({ title, image, price, rating}) {
+  return (
+    <div className='product'>
+        <div className='product__info'>
+            <p>{title}</p>
+            <p className='product__price'>
+                <small>₹</small>
+                <strong>{price}</strong>
+            </p>
+            <div className='product__rating'>
+                {Array(rating).fill().map((_, i) => (<p>🌟</p>))}
+            </div>
+        </div>
+
+        <img src={image} alt=''></img>
+        <button>Add to Basket</button>
+      
+    </div>
+  )
+}
+
+export default Product;
