b47ab94f7fc68a8fdbeb0ec84d1e291d8c814b43
adithyaabinoy <adithyaabinoy@gmail.com>
2023-06-11 10:41:43 +0530

    paymentmethod

--- amazon-clone/src/Payment.js
+++ amazon-clone/src/Payment.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Payment.css'
 import { useStateValue } from './StateProvider'
 import CheckoutProduct from './CheckoutProduct';
 import { Link } from "react-router-dom";
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from './reducer';
 
 function Payment() {
     const [{ basket, user }, dispatch] = useStateValue();
+
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+
+    const handleSubmit = e => {
+        //stripe 
+    }
+
+    const handleChange = event => {
+        /* listen for changes inside CardElement and dispaly any errors
+        as the customer types in their card details */
+        setDisabled(event.empty);
+        setError(event.error ? event.error.message : "");
+    }
+
+
   return (
     <div className='payment'>
         <div className='payment__container'>
@@ -53,6 +75,21 @@ function Payment() {
                 </div>
                 <div className='payment__details'>
                     {/* Stripe */}
+
+                    <form onSubmit={handleSubmit}>
+                        <CardElement onChange={handleChange} />
+
+                        <div className='payment__priceContainer'>
+                            <CurrencyFormat
+                               renderText={(value) => <h3>Order Total :{value}</h3>}
+                               decimalScale={2}
+                               value={getBasketTotal(basket)}
+                               displayType={"text"}
+                               // thousandSeparator={true}
+                               prefix={"₹"}
+                            />
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
